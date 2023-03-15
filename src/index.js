6e006f0a091bf56dfb4ab6207c6cefb0a8170a0a
birendra8757 <computersinghaniya@gmail.com>
2023-03-15 10:21:18 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+
+require("dotenv").config();
 const route = require('./Route/route.js');
 const mongoose = require('mongoose');
 const app = express();
-
+const PORT=process.env.PORT ||3000
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://birendra:Kumar123@cluster0.enhlifs.mongodb.net/Edayachandran", {
@@ -12,6 +15,6 @@ mongoose.connect("mongodb+srv://birendra:Kumar123@cluster0.enhlifs.mongodb.net/E
 .catch(err => console.log(err))
 app.use('/', route)
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Express app running on port ' + (3000))
 });
